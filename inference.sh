d8075b340bb572f8de242d199cd83448c1543561
Galway <453213173@qq.com>
2023-10-22 05:29:09 +0800

    fix: change .jpg to .png to remove noises in results

--- inference.sh
+++ inference.sh
@@ -2,3 +2,9 @@
 CUDA_VISIBLE_DEVICES=6 python /home/ubuntu/YX/code/Graphonomy/exp/inference/inference.py --loadmodel /home/ubuntu/YX/code/Graphonomy/data/pretrained_model/inference.pth --img_path /home/ubuntu/YX/code/virtual_tryon/StableTryOn/datasets/DeepFashion/after_split_512_384/test/images --output_path /home/ubuntu/YX/code/virtual_tryon/StableTryOn/datasets/DeepFashion/after_split_512_384/test/segmentation --output_name /over_written
 
 CUDA_VISIBLE_DEVICES=7 python /home/ubuntu/YX/code/Graphonomy/exp/inference/inference.py --loadmodel /home/ubuntu/YX/code/Graphonomy/data/pretrained_model/inference.pth --img_path /home/ubuntu/YX/code/virtual_tryon/StableTryOn/datasets/DeepFashion/after_split_512_384/train/images --output_path /home/ubuntu/YX/code/virtual_tryon/StableTryOn/datasets/DeepFashion/after_split_512_384/train/segmentation --output_name /over_written
+
+
+
+
+
+CUDA_VISIBLE_DEVICES=6 python /home/ubuntu/YX/code/Graphonomy/exp/inference/inference.py --loadmodel /home/ubuntu/YX/code/Graphonomy/data/pretrained_model/inference.pth --img_path /home/ubuntu/YX/code/virtual_tryon/StableTryOn/datasets/DeepFashion/after_split_512_384/test/images --output_path ./ --output_name /over_written
